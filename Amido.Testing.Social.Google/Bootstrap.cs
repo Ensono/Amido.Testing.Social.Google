e651850a51d0acf758932f59d6fd63b417eba06e
seanmcalinden <sean.f.mcalinden@live.co.uk>
2014-07-14 22:26:44 +0100

    Added bootstrap

---
+++ Amido.Testing.Social.Google/Bootstrap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Amido.Testing.Social.Google.PageObjects;
+
+using OpenQA.Selenium;
+
+namespace Amido.Testing.Social.Google
+{
+    public static class Bootstrap
+    {
+        public static void Init(Func<IWebDriver> webDriver)
+        {
+            GoogleAllowAppPageObject.Init(webDriver);
+        } 
+    }
+}
