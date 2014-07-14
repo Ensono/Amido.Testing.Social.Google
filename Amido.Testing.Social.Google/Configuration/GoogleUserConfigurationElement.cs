0a5e8976190bc7349c89e5aafb58b44273f37ddb
seanmcalinden <sean.f.mcalinden@live.co.uk>
2014-07-14 22:16:37 +0100

    Init proj

---
+++ Amido.Testing.Social.Google/Configuration/GoogleUserConfigurationElement.cs
@@ -0,0 +1,58 @@
+﻿using System.Configuration;
+
+namespace Amido.Testing.Social.Google.Configuration
+{
+    public class GoogleUserConfigurationElement : ConfigurationElement
+    {
+        [ConfigurationProperty("email", IsKey = true, IsRequired = true)]
+        public string Email
+        {
+            get
+            {
+                return this["email"].ToString();
+            }
+            set
+            {
+                this["email"] = value;
+            }
+        }
+
+        [ConfigurationProperty("password", IsRequired = true)]
+        public string Password
+        {
+            get
+            {
+                return this["password"].ToString();
+            }
+            set
+            {
+                this["password"] = value;
+            }
+        }
+
+        [ConfigurationProperty("dateOfBirth", IsRequired = false)]
+        public string DateOfBirth
+        {
+            get
+            {
+                return this["dateOfBirth"].ToString();
+            }
+            set
+            {
+                this["dateOfBirth"] = value;
+            }
+        }
+        [ConfigurationProperty("gender", IsRequired = false)]
+        public string Gender
+        {
+            get
+            {
+                return this["gender"].ToString();
+            }
+            set
+            {
+                this["gender"] = value;
+            }
+        }
+    }
+}
---
+++ Amido.Testing.Social.Google/PageObjects/GoogleAllowAppPageObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using Amido.WebDriver.Utilities.Extensions;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Amido.Testing.Social.Google.PageObjects
+{
+    public class GoogleAllowAppPageObject
+    {
+        private static Func<IWebDriver> webDriver;
+
+        public static void Init(Func<IWebDriver> getWebDriver)
+        {
+            if (webDriver == null)
+            {
+                throw new Exception("You must call GoogleAllowAppPageObject.Init once before using this class");
+            }
+
+            webDriver = getWebDriver;
+        }
+
+        public GoogleAllowAppPageObject()
+        {
+            PageFactory.InitElements(webDriver(), this);
+        }
+
+        [FindsBy(How = How.Name, Using = "Email")]
+        public IWebElement Email { get; set; }
+
+        [FindsBy(How = How.Name, Using = "Passwd")]
+        public IWebElement Password { get; set; }
+
+        [FindsBy(How = How.Id, Using = "signIn")]
+        public IWebElement SignInAllow { get; set; }
+
+        public bool IsLoaded()
+        {
+            return this.SignInAllow.WaitUntil(e => e.Displayed).Displayed;
+        }
+    }
+}
